c91e6ecaa270fa81867027ea4d2bcc0e56245ffc
lovisgod <olifedayo94@gmail.com>
2020-11-30 04:37:04 +0100

    feature:started project and built landing page

---
+++ android/app/src/main/kotlin/com/lovisgod/food_delivery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lovisgod.food_delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/screens/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Food Delivery UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          scaffoldBackgroundColor: Colors.grey.shade50,
+          primaryColor: Colors.deepOrangeAccent),
+      home: HomeScreen(),
+    );
+  }
+}
